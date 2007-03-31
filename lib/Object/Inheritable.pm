de425680611dcb6d99c3ae02201f4b09207ee90e
Jos Boumans <kane@cpan.org>
2007-03-31 17:36:09 +0000

    r3900@rimmer:  kane | 2007-03-31 18:23:38 +0200
    git-svn-id: http://oss.dwim.org/object-inheritable@1819 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/Object/Inheritable.pm
+++ lib/Object/Inheritable.pm
@@ -206,20 +206,19 @@ Or, and this also works for other classes than your own as well:
     sub ___get_cache { return \%Cache };
 }
 
+=head1 BUG REPORTS
+
+Please report bugs or other issues to E<lt>bug-object-inheritable@rt.cpan.org<gt>.
+
 =head1 AUTHOR
 
-This module by
-Jos Boumans E<lt>kane@cpan.orgE<gt>.
+This module by Jos Boumans E<lt>kane@cpan.orgE<gt>.
 
 =head1 COPYRIGHT
 
-This module is
-copyright (c) 2006 Jos Boumans E<lt>kane@cpan.orgE<gt>.
-All rights reserved.
+This library is free software; you may redistribute and/or modify it 
+under the same terms as Perl itself.
 
-This library is free software;
-you may redistribute and/or modify it under the same
-terms as Perl itself.
 
 =cut
 
